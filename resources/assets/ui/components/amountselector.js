01db765baee24959d4a264a6311191a6429085fc
Stef van den Berg <stef@debmedia.nl>
2015-12-23 10:03:10 +0100

    Added the 0.25 as option to book the hours

--- resources/assets/ui/components/amountselector.js
+++ resources/assets/ui/components/amountselector.js
@@ -25,7 +25,7 @@ var AmountSelector = React.createClass({
   },
   render: function() {
     var options = (
-      [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8].map((amount) => {
+      [0, 0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8].map((amount) => {
         return <option value={amount} selected={this.state.value == amount}>{amount}</option>
       })
     );
